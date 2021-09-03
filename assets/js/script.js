ffb6008858a86361a0cf9e12d7e5c3a40033b5eb
Michelle Wehr <michelle_wehr@icloud.com>
2021-09-02 20:28:55 -0400

    cleared previous search results git add .

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,70 +6,6 @@ const popularMovieApi = 'https://api.themoviedb.org/3/movie/popular' + movieApiK
 //get movie search button from document
 const movieSearchBtn = document.querySelector(".movieSearchBtn");
 
-function searchMovieByTitle(title) {
-    let movieTitle = $('#searchMovie').val().trim();
-    let movieApiUrl = 'https://api.themoviedb.org/3/search/movie' + movieApiKey + '&query=' + movieTitle;
-    if (!movieTitle) {
-        return;
-    }
-    fetch(movieApiUrl) 
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        // let resultArr = [];
-        let resultArr = response.results;
-        console.log(resultArr)
-        for (let i = 0; i < resultArr.length; i++) {
-            let movieId = response.results[i].id;
-            fetch('https://api.themoviedb.org/3/movie/' + movieId + movieApiKey)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(response) {
-                console.log(response)
-                //document element that will hold the movie posters
-                let moviesEl = document.querySelector(".posters")
-                //create a poster div for each movie
-                let moviePoster = document.createElement("div");
-                moviePoster.classList = 'column is-one-fifth moviePosterDiv';
-                //create the image element
-                let posterImg = document.createElement("img");
-                posterImg.src = 'https://image.tmdb.org/t/p/original' + response.poster_path;
-                //append movie poster div to document element
-                moviesEl.appendChild(moviePoster);
-                // append image element to poster div
-                moviePoster.appendChild(posterImg);
-                // create a clickable favorite star element
-                let favoriteAnchor = document.createElement("a");
-                // create the star icon element
-                let favoriteIcon = document.createElement("i");
-                favoriteIcon.classList = 'far fa-star fa-large star';
-                // append the anchor to the movie poster
-                moviePoster.appendChild(favoriteAnchor);
-                // append the icon to the anchor 
-                favoriteAnchor.appendChild(favoriteIcon);
-                // get release date
-                let releaseDate = response.release_date;
-                // format the date into just the release year
-                let year = moment(releaseDate, "YYYY-MM-DD").format('YYYY');
-                //create span to hold the release year
-                let yearEl = document.createElement("span");
-                yearEl.textContent = "Released: " + year;
-                //append the year to the movie poster div
-                moviePoster.appendChild(yearEl);
-                // create movie rating span
-                let movieRating = document.createElement('span');
-                // add "rating" class so that there is space between the 2 spans
-                movieRating.classList = 'rating';
-                movieRating.textContent = response.vote_average + ' /10';
-                //append movie rating to the poster div
-                moviePoster.appendChild(movieRating);
-            })
-        }
-    })
-}
-
 //POPULAR MOVIES TRAILER
 function showNewReleases() {
     //fetch to get popular movie ids
@@ -149,6 +85,72 @@ function showNewReleases() {
         }
         })
 }
+
+function searchMovieByTitle(title) {
+    $(".posters").html("");
+    let movieTitle = $('#searchMovie').val().trim();
+    let movieApiUrl = 'https://api.themoviedb.org/3/search/movie' + movieApiKey + '&query=' + movieTitle;
+    if (!movieTitle) {
+        return;
+    }
+    fetch(movieApiUrl) 
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        // let resultArr = [];
+        let resultArr = response.results;
+        console.log(resultArr)
+        for (let i = 0; i < resultArr.length; i++) {
+            let movieId = response.results[i].id;
+            fetch('https://api.themoviedb.org/3/movie/' + movieId + movieApiKey)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(response) {
+                console.log(response)
+                //document element that will hold the movie posters
+                let moviesEl = document.querySelector(".posters")
+                //create a poster div for each movie
+                let moviePoster = document.createElement("div");
+                moviePoster.classList = 'column is-one-fifth moviePosterDiv';
+                //create the image element
+                let posterImg = document.createElement("img");
+                posterImg.src = 'https://image.tmdb.org/t/p/original' + response.poster_path;
+                //append movie poster div to document element
+                moviesEl.appendChild(moviePoster);
+                // append image element to poster div
+                moviePoster.appendChild(posterImg);
+                // create a clickable favorite star element
+                let favoriteAnchor = document.createElement("a");
+                // create the star icon element
+                let favoriteIcon = document.createElement("i");
+                favoriteIcon.classList = 'far fa-star fa-large star';
+                // append the anchor to the movie poster
+                moviePoster.appendChild(favoriteAnchor);
+                // append the icon to the anchor 
+                favoriteAnchor.appendChild(favoriteIcon);
+                // get release date
+                let releaseDate = response.release_date;
+                // format the date into just the release year
+                let year = moment(releaseDate, "YYYY-MM-DD").format('YYYY');
+                //create span to hold the release year
+                let yearEl = document.createElement("span");
+                yearEl.textContent = "Released: " + year;
+                //append the year to the movie poster div
+                moviePoster.appendChild(yearEl);
+                // create movie rating span
+                let movieRating = document.createElement('span');
+                // add "rating" class so that there is space between the 2 spans
+                movieRating.classList = 'rating';
+                movieRating.textContent = response.vote_average + ' /10';
+                //append movie rating to the poster div
+                moviePoster.appendChild(movieRating);
+            })
+        }
+    })
+}
+
 // call showNewRelease function to run on page load
 showNewReleases();
 // when the favorite star is clicked on it will change to a solid star
